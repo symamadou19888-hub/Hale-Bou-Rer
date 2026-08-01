d13ab1d294c0521120d1f757df2f254266aaf3a0
Sy Mamadou <symamadou19888@gmail.com>
2026-08-01 01:14:53 +0000

    Ajout service worker et manifest PWA

---
+++ static/service-worker.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', function(event) {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(fetch(event.request));
+});
