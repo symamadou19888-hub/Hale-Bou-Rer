c02cb5e3f62e46a89ffd5ba27a6af039ad8ea662
Sy Mamadou <symamadou19888@gmail.com>
2026-08-04 17:13:32 +0000

    Ajout gestion notifications dans service worker

--- static/service-worker.js
+++ static/service-worker.js
@@ -6,6 +6,16 @@ self.addEventListener('activate', function(event) {
     event.waitUntil(clients.claim());
 });
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(fetch(event.request));
+self.addEventListener('push', function(event) {
+    const data = event.data ? event.data.json() : {};
+
+    const titre = data.title || "Halé Bou Rér";
+    const options = {
+        body: data.body || "Nouveau signalement disponible",
+        icon: "/static/images/logo.png"
+    };
+
+    event.waitUntil(
+        self.registration.showNotification(titre, options)
+    );
 });
